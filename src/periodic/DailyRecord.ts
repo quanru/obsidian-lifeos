94515aa8a0fda3d8905cd63fd08dc6b8e7fc3355
linyibing <linyibing@bytedance.com>
2024-11-16 23:53:53 +0800

    fix: log level revert

--- src/periodic/DailyRecord.ts
+++ src/periodic/DailyRecord.ts
@@ -90,7 +90,7 @@ export class DailyRecord {
         `${
           getI18n(this.locale)[`${ERROR_MESSAGE}FAILED_GET_USEMEMOS_VERSION`]
         }`,
-        LogLevel.warn,
+        LogLevel.error,
       );
     }
   }
@@ -145,7 +145,7 @@ export class DailyRecord {
         `${
           getI18n(this.locale)[`${ERROR_MESSAGE}DAILY_RECORD_FETCH_FAILED`]
         }: ${error}`,
-        LogLevel.warn,
+        LogLevel.error,
       );
     }
   }
@@ -191,7 +191,7 @@ export class DailyRecord {
         `${
           getI18n(this.locale)[`${ERROR_MESSAGE}RESOURCE_FETCH_FAILED`]
         }: ${error}`,
-        LogLevel.warn,
+        LogLevel.error,
       );
     }
   }
@@ -324,7 +324,7 @@ export class DailyRecord {
               `${
                 getI18n(this.locale)[`${ERROR_MESSAGE}NO_DAILY_FILE_EXIST`]
               } ${today}`,
-              LogLevel.warn,
+              LogLevel.error,
             );
           }
         }
